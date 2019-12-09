72d7a4709c3a1fdfd7dd5b9d6c702d2526dfd99b
zoufangbo <zoufangbo6690@ipalfish.com>
2019-12-09 18:48:11 +0800

    [Add][V1.0.[Add][V1.0.3]Study basic use
    1.Deal with router
    2.Add navigation bar use
    3.Component use
    
    Signed-off-by: zoufangbo <zoufangbo6690@ipalfish.com>

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
 <!--    <img src="./assets/logo.png">-->
+    <!--引用组件的方式-->
+    <IndexPage></IndexPage>
     <router-view/> <!--路由展示-->
   </div>
 </template>
 
 <script>
+import IndexPage from './components/IndexPage'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {IndexPage}
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Router from 'vue-router'
 import Index from '../components/Index'
 import content from '../page/content'
 import IndexPage from '../components/IndexPage'
+import Login from '../components/Common/Login'
 
 Vue.use(Router)
 
@@ -14,7 +15,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/login'
+      redirect: '/HelloWorld'
     },
     {
       path: '/index',
@@ -23,6 +24,14 @@ export default new Router({
         title: '首页'
       },
       component: Index// 设置跳转的页面
+    },
+    {
+      path: '/Login',
+      name: 'login', // path别名，可没有
+      meta: {
+        title: '登录'
+      },
+      component: Login// 设置跳转的页面
     },
     {
       path: '/indexPage',
